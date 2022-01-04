aeebf598c61ef1f9fce33cdd3139439af1642469
Ahmed <darwish144.ad@gmail.com>
2022-01-04 08:46:06 +0300

    input autocomplete done

--- index.js
+++ index.js
@@ -12,70 +12,32 @@ const fetchData = async (searchTerm) => {
     return response.data.Search;
 };
 
-const root = document.querySelector('.autocomplete');
-root.innerHTML = `
-    <label><b> Search for a Movie</b> </label>
-    <input class="input"/>
-    <div class="dropdown">
-    <div class="dropdown-menu">
-      <div class="dropdown-content results "></div> 
-    </div>
-  </div>
-`;
-
-
-const input = document.querySelector('input');
-const dropdown = document.querySelector('.dropdown');
-const resultsWrapper = document.querySelector('.results');
-
-const onInPut = async event => {
-   
-const movies = await fetchData(event.target.value);
-if(!movies.length) {
-    dropdown.classList.remove('is-active')
-}
-
-resultsWrapper.innerHTML = '';
-dropdown.classList.add('is-active');
-   for (let movie of movies) {
-       const movSelection = document.createElement('a');
-       const imgSrc = movie.Poster === 'N/A' ?  '' : movie.Poster;
-
-       
-       movSelection.classList.add('dropdown-item')
-       movSelection.innerHTML = `
-        <img src ="${imgSrc}"/img>
-        ${movie.Title}  
-       `; 
-       movSelection.addEventListener('click',() => {
-          dropdown.classList.remove('is-active') 
-          input.value = movie.Title;
-          onMovieSelect(movie);
-       }) 
-       resultsWrapper.appendChild(movSelection);       
-   }
-
-
-};
-
-input.addEventListener('input',debounce(onInPut,500));
-
-document.addEventListener('click', event => {
-   if(!root.contains(event.target)) {
-    dropdown.classList.remove('is-active');
-   }   
+createAutocomplete({
+  root : document.querySelector('.autocomplete'),
+  renderOption(movie) {
+  const imgSrc = movie.Poster === 'N/A' ?  '' : movie.Poster;
+  return  `
+  <img src ="${imgSrc}"/img>
+  ${movie.Title} (${movie.Year})
+ `; 
+  },
+  onOptionSelect(movie) {
+    onMovieSelect(movie);
+  },
+  inputValue(movie) {
+    return movie.Title;
+  }
 });
-
 const onMovieSelect = async movie =>  {
-    const response = await axios.get('http://www.omdbapi.com',
-    {
-         params: {
-            apikey: 'ccfc0141',
-            i: movie.imdbID
-        }
-    });
-    document.querySelector('#summery').innerHTML = movieTemplate(response.data);
-    
+  const response = await axios.get('http://www.omdbapi.com',
+  {
+       params: {
+          apikey: 'ccfc0141',
+          i: movie.imdbID
+      }
+  });
+  document.querySelector('#summery').innerHTML = movieTemplate(response.data);
+  
 };
 
 const movieTemplate = (movieDetail) => {
